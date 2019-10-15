dba192accfda43c3c26dec5d83672b6670164b8f
bond <ilkerzaimoglu@gmail.com>
2019-10-15 04:52:28 -0700

    challange4 done

--- code-challenges/forEach/challenges-01.test.js
+++ code-challenges/forEach/challenges-01.test.js
@@ -72,8 +72,10 @@ CHALLENGE 4
 Write a function named removeWithForEach that produces the same output as challenge 3, but uses forEach.
 ------------------------------------------------------------------------------------------------ */
 
-const removeWithForEach = (arr, callback) => {
-  // Solution code here...
+const removeWithForEach = (arr, callback) => { arr.forEach (num => callback (arr[num], arr));
+  
+  return arr;
+  
 };
 
 /* ------------------------------------------------------------------------------------------------
