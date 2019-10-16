d93e4597b891a3dc234fc82abb929f63a7eeb476
bond <ilkerzaimoglu@gmail.com>
2019-10-16 05:12:11 -0700

    completed

--- code-challenges/value-vs-reference/challenges-02.test.js
+++ code-challenges/value-vs-reference/challenges-02.test.js
@@ -65,7 +65,7 @@ console.log(people[1].isAuthor) prints true
 ------------------------------------------------------------------------------------------------ */
 
 const setStatusAsAuthor = (people) => {
-  // Solution code here...
+  people.forEach(property =>{property.isAuthor = true;})
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -84,7 +84,7 @@ console.log(a) prints [1, 2, 3, 4]
 ------------------------------------------------------------------------------------------------ */
 
 const append = (arr1, arr2) => {
-  // Solution code here...
+  arr2.forEach(object => {arr1.push(object)});
 
 };
 
