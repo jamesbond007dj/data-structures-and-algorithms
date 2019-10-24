9283ccbab395119d15d77ffb6ea5406a147de640
bond <ilkerzaimoglu@gmail.com>
2019-10-24 06:24:24 -0700

    cc8 third done

--- code-challenges/filter/challenges-08.test.js
+++ code-challenges/filter/challenges-08.test.js
@@ -37,14 +37,14 @@ const filterStringsWithVowels = (arr) => {
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
 
-Write a function named notInFirstArray that, given two arrays as input, uses filter to return an array of all the elements in the second array that are not included in the first array.
+Write a function named notInFirstArray that, given two arrays as input,
+uses filter to return an array of all the elements in the second array that are not included in the first array.
 
 For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 ------------------------------------------------------------------------------------------------ */
 
-const notInFirstArray = (forbiddenValues, arr) => {
-  // Solution code here...
-};
+const notInFirstArray = (forbiddenValues, arr) => arr.filter(elements => !forbiddenValues.includes(elements));
+
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
